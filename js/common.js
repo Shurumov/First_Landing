6a22de75100ac468c6f35faadba442e7c26e0547
Aleksei <alexei0113@gmail.com>
2017-09-30 18:11:42 +0700

    Add adaptive

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".main-header-mobile-toggler").click(function(){
+        $(".main-header-mobile-toggler").toggleClass("on");
+        $(".mobile-menu").slideToggle();
+        
+    });
+    
+});
